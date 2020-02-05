a1c89c7e885273922941fe26e5defa9719fc53d8
SantiagoGGomila <60632347+SantiagoGGomila@users.noreply.github.com>
2020-02-05 10:23:06 -0300

    ejercicio 7 suma
    al resto lo copio tal cual y cambio los signos

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,36 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
-}
+  var primerNumero;
+  var segundoNumero;
+  var resultado
 
+  primernumero=document.getElementById('numeroUno').value;
+  console.log(primerNumero);
+   primernumero=parseInt(primerNumero);
+  segundonumero=document.getElementById('numeroDos').value;
+  console.log(segundoNumero);	
+  segundonumero=parseInt(segundoNumero);
+
+ 	resultado= primernumero+segundonumero;
+
+
+ 	console.log(resultado);
+}
 function restar()
 {
-	
+   
 }
 
 function multiplicar()
 { 
-	
+    
+
+    
 }
 
 function dividir()
 {
-	
-}
+   
 
+}
