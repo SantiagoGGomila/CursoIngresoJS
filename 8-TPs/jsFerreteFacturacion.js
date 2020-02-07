ffb4ffc7dacf86d8760cd9c58b83c1afc048524f
SantiagoGGomila <60632347+SantiagoGGomila@users.noreply.github.com>
2020-02-06 21:49:42 -0300

    TP FACTURACION RESUELTO
    .

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,84 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var precioUno;
+	var precioDos;
+	var tercerPrecio;
+	var resultado;
+	
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioUno=parseInt(precioUno);
+
+	precioDos=document.getElementById('PrecioDos').value;
+	precioDos=parseInt(precioDos);
+
+	tercerPrecio=document.getElementById('PrecioTres').value;
+	tercerPrecio=parseInt(tercerPrecio);
+
+	resultado=precioUno+precioDos+tercerPrecio;
+
+	alert("su resultado es "+ resultado);
+
 	
 }
 function Promedio () 
 {
+	var precioUno;
+	var precioDos;
+	var tercerPrecio;
+	var resultado;
+	var promedio
+
+	
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioUno=parseInt(precioUno);
+
+	precioDos=document.getElementById('PrecioDos').value;
+	precioDos=parseInt(precioDos);
+
+	tercerPrecio=document.getElementById('PrecioTres').value;
+	tercerPrecio=parseInt(tercerPrecio);
+
+	resultado=precioUno+precioDos+tercerPrecio;
+
+	promedio=resultado/3
+
+	alert("su resultado es "+ promedio);
+
 	
 }
 function PrecioFinal () 
 {
+	var precioUno;
+	var precioDos;
+	var tercerPrecio;
+	var resultado;
+	var iva;
+	var impuestoaplicado;
+
+	
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioUno=parseInt(precioUno);
+
+	precioDos=document.getElementById('PrecioDos').value;
+	precioDos=parseInt(precioDos);
+
+	tercerPrecio=document.getElementById('PrecioTres').value;
+	tercerPrecio=parseInt(tercerPrecio);
+
+
+	resultado=precioUno+precioDos+tercerPrecio;
+
+	iva=21/100;
+
+	impuestoaplicado=resultado+resultado*iva;
+
+	alert(impuestoaplicado);
+
+
+
 	
 }
