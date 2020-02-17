48214092d696cb5262860e3cd3e118069e57ebad
SantiagoGGomila <60632347+SantiagoGGomila@users.noreply.github.com>
2020-02-17 20:04:18 -0300

    ejercicio 5 actualizado y terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,65 @@
 function mostrar()
 {
+	var planeta;
+
+	
+
+
+	planeta=prompt("ingrese el planeta");
+
+	switch(planeta)
+	{
+		case 'mercurio':
+
+		case 'venus':
+		console.log("aca hace calor");
+		break
+
+		case 'tierra':
+		
+		console.log("aca vivo");
+		
+		break
+
+		case 'marte':
+		
+		
+
+		case 'jupiter':
+		console.log("aca hace mas frio");
+		break
+
+		default:
+		console.log("esto no es un planeta salame");
+
+		
+	}
+	
+	
+		
 
 }
+
+/*
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+ */   
