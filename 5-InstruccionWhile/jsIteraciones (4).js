1e1a5f31215460f2fc7d7b1f0c41b27b388a8284
alumno <alumno>
2020-02-19 12:08:17 -0300

    while ejercicios terminados explicacion NaN

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,10 +2,11 @@ function mostrar()
 {
 
 	var numero;
-	
+	numero=parseInt(numero);
 	numero=prompt("ingrese un número entre 0 y 9.");
+	//valorDeVerdad=isNaN() todo lo que no es un numero, me va a evaluar el conjunto de verdad
 
-	while(numero<0 || numero>10)
+	while(isNaN(numero)||numero< 0 || numero>10)
 	{
 		numero=prompt("ingrese un número entre 0 y 9.");
 	}
