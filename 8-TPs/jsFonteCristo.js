76dbf1c7333e9d58d8bab5537b0dde2080b90f60
SantiagoGGomila <60632347+SantiagoGGomila@users.noreply.github.com>
2020-03-02 16:57:42 -0300

    fonte cristo terminado
    problema de syntax en linea 126 (no se como resolverlo)

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -7,7 +7,120 @@ c.	Se pedirán un número positivo y se mostrará la cantidad de números divisi
 d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
 e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
 */
-function ComenzarIngreso () 
+function NumerosPares () 
 {
+	var numero;
+	var contadorPar;
+
+	numero=document.getElementById('numero').value;
+	numero=parseInt(numero);
+
+	contadorPar=0
+
+	for(numero=numero;numero>0;numero--)
+	{
+		if(numero%2==0)
+		{
+			console.log("su numero es"+numero+"Y es par");
+			contadorPar++
+		}
+	}
  	
+}
+function NumerosImpares ()
+{
+	var	numero;
+	var contadorImpar;
+
+	numero=document.getElementById('numero').value;
+	numero=parseInt(numero);
+
+	contadorImpar=0
+
+	for(numero=numero;numero>0;numero--)
+	{
+		if(numero%2!=0)
+		{
+			console.log("el numero es "+numero+ "y es impar")
+			contadorImpar++
+		}
+	}
+
+
+}
+function NumerosDivisibles()
+{
+	var numero;
+	var dividendo;
+
+	numero=document.getElementById('numero').value;
+	numero=parseInt(numero);
+
+	for(dividendo=1;dividendo<101;dividendo++)
+	{
+		if(dividendo%numero==0)
+		{
+			console.log("el numero "+dividendo+"se divide por "+ numero);
+		}
+	}
+
+}
+function VerificarPrimo ()
+{
+	var numero;
+	var i;
+	var bandera;
+
+	numero=document.getElementById('numero').value;
+	numero=parseInt(numero);
+
+	bandera=0;
+
+	for(i=2;i<numero;i++)
+	{
+		if(numero%i==0)
+		{
+			bandera=1;
+			break;
+		}
+	}
+	if(bandera==0)
+	{
+		console.log("el numero primo es"+numero);
+	}else
+	{
+		console.log(numero+"no es primo");
+	}
+	
+}
+
+function NumerosPrimos () 
+{
+	var numero;
+	var i;
+	var bandera;
+	var contador
+
+	numero=document.getElementById('numero').value;
+	numero=parseInt(numero);
+
+	bandera=0;
+	contador=0
+
+	for(numero=numero;numero>-1;numero--)
+	{
+		for(i=2;i<numero;i++)
+		{
+			if(numero%i==0)
+		{
+			bandera=1;
+			break;
+		}
+	}
+	if(bandera==0)
+	{
+		console.log("el numero primo es"+numero);
+		contador++;
+		console.log("los primos son"+ contador)
+	}
 }
